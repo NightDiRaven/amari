16bed8e312343f640500b8aff317b9c77650b5b0
Ivan Dolgov <ddprog92@gmail.com>
2016-02-18 12:22:21 +0300

    Some changes

---
+++ src/Amari/App.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IN
+ * Date: 18.02.2016
+ * Time: 12:15
+ */
+
+namespace Amari;
+
+
+class App {
+	public static function world()
+	{
+		return 'Hello World, Composer!';
+	}
+}
