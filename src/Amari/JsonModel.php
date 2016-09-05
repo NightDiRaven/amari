2b9e740eb61cc766039e97119535e98af994c5bc
Ivan <ivan@os-design.ru>
2016-09-05 16:25:02 +0300

    Changes

---
+++ src/Amari/JsonModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Amari;
+
+
+
+use Amari\Jsonable\Traits\Jsonable;
+use Amari\Translatable\Contracts\JsonableContract;
+use Amari\Translatable\Contracts\TranslatableContract;
+use Amari\Translatable\Traits\Translatable;
+
+abstract class JsonModel extends Model implements JsonableContract, TranslatableContract {
+	use Jsonable, Translatable {
+		Jsonable::setAttribute as jsonSetAttribute;
+		Jsonable::getAttribute as jsonGetAttribute;
+		Translatable::setDirtyAttribute as translateSetAttribute;
+		Translatable::getTransAttribute as translateGetAttribute;
+	}
+
+	public function getAttribute($key) {
+		return $this->translateGetAttribute($key) ??  $this->jsonGetAttribute($key);
+	}
+
+	public function setAttribute($key, $value) {
+		return $this->translateSetAttribute($key, $value) ?? $this->jsonSetAttribute($key, $value);
+	}
+}
